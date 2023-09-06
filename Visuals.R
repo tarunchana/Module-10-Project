81e0186d2d791f3348458a9fcd85925964bf39e9
Tarun Chana <tarun.chana@gad.gov.uk>
2023-09-06 17:37:15 +0100

    Energy inflation G7 countries graph

--- Visuals.R
+++ Visuals.R
@@ -2,13 +2,10 @@
 
 # Import packages
 library(tidyverse) # contains ggplot2, tidyr, dplyr and readr
-library(ggplot2)
 library(janitor) # To clean column names
-library(scales) # Methods for automatically determining labels for axes
-library(patchwork) # Used to combine multiple plots
-library(gt) # For creating tables 
 library(reshape2)
 
+# Graph of CPI inflation since the pandemic compared against other major G7 countries  
 G7_data <- read.csv("Data/The_increase_in_the_price_level_since_the_pandemic_in_the_UK_is_the_highest_across_the_G7.csv", header = TRUE, skip = 8)
 names(G7_data)[1] = "Date"
 G7_data <- melt(G7_data, id.vars = "Date")
@@ -16,9 +13,39 @@ names(G7_data)[2] = "Country"
 G7_data$Date <- as.Date(paste0("01-", G7_data$Date), format = "%d-%b-%y")
 
 G7_graph <- ggplot(data = G7_data, aes(x = Date, y = value, color = Country, group = Country)) +
-  geom_line() +
+  geom_line(size = 0.8) +
   geom_line(data = subset(G7_data, Country == "UK"), size = 1.5) +
-  labs(x = "Date", y = "CPI", title = "CPI Inflation across major G7 Countries since the Pandemic") +
+  scale_color_manual(values = c("UK" = "red",
+                                "France" = "blue",
+                                "Germany" = "green",
+                                "Italy" = "purple",
+                                "Japan" = "orange",
+                                "US" = "black",
+                                "Canada" = "pink"), guide = guide_legend(title = "Country")) +
+  labs(x = "Date (Year)", y = "CPI",
+       title = expression(bold(underline("CPI Inflation across major G7 Countries since the Pandemic")))) +
   theme_minimal()
-plot(G7_graph)
+
+# Graph of energy inflation since the pandemic compared against other major G7 countries 
+G7_energy <- read.csv("Data/UK_energy_price_inflation_was_the_highest_across_major_advanced_economies_in_March_2023.csv", header = TRUE, skip = 6)
+names(G7_energy)[1] = "Date"
+G7_energy <- melt(G7_energy, id.vars = "Date")
+names(G7_energy)[2] = "Country"
+G7_energy$Date <- as.Date(paste0("01-", G7_energy$Date), format = "%d-%b-%y")
+
+G7_nrg_graph <- ggplot(data = G7_energy, aes(x = Date, y = value, color = Country, group = Country)) +
+  geom_line(size = 0.8) +
+  geom_line(data = subset(G7_energy, Country == "UK"), size = 1.5) +
+  scale_color_manual(values = c("UK" = "red",
+                                "France" = "blue",
+                                "Germany" = "green",
+                                "Italy" = "purple",
+                                "Japan" = "orange",
+                                "US" = "black",
+                                "Canada" = "pink"), guide = guide_legend(title = "Country")) +
+  labs(x = "Date (Year)", y = "12-Month Rate (%)", 
+       title = expression(bold(underline("12-Month Rate of Energy Inflation across major G7 Countries since the Pandemic")))) +
+  theme_minimal()
+plot(G7_nrg_graph)
+
 
